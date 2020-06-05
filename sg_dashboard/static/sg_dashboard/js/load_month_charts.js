48f10731faa595e7f26516ccea0106163861a2c8
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-06-05 02:22:45 +0100

    copied JS to HTML

--- sg_dashboard/static/sg_dashboard/js/load_month_charts.js
+++ sg_dashboard/static/sg_dashboard/js/load_month_charts.js
@@ -2,23 +2,25 @@
 var charts = {};
 
 $(function () {
-    var params = window.location.pathname.split('/');
-    var year = params['3'];
-    var month = params['4'];
+    var params = window.location.pathname.split("/");
+    var year = params["3"];
+    var month = params["4"];
     // DATA url
-    var endpoint = `http://localhost:8000/dashboard/api/${year}/${month}/`
+    var endpoint = `http://localhost:8000/dashboard/api/${year}/${month}/`;
     console.log(endpoint);
     /**
      * Create charts on the dashboard
      */
     $.ajax({
-        method: 'GET',
+        method: "GET",
         url: endpoint,
         success: function (data) {
             // Days labels
-            var labels = new Array(data.Days.length).fill(1).map((_, i) => 'Day '.concat(i + 1));
-            min_temp_data = [];    
-            avg_temp_data = [];    
+            var labels = new Array(data.Days.length)
+                .fill(1)
+                .map((_, i) => "Day ".concat(i + 1));
+            min_temp_data = [];
+            avg_temp_data = [];
             max_temp_data = [];
             pressure_data = [];
             humid_data = [];
@@ -32,36 +34,36 @@ $(function () {
             });
             //  Temperature chart
             var tempdata = {
-                'min_data': min_temp_data,
-                'avg_data': avg_temp_data,
-                'max_data': max_temp_data,
-                'labels': labels,
+                min_data: min_temp_data,
+                avg_data: avg_temp_data,
+                max_data: max_temp_data,
+                labels: labels,
             };
             charts.tempChart = createTempChart(tempdata);
             // Set min temp
             min_temp = data.Month[0].min_temp;
-            $('#min_temp').html(min_temp + '<sup>°C</sup>');
+            $("#min_temp").html(min_temp + "<sup>°C</sup>");
             // Set avg temp
             avg_temp = data.Month[0].avg_temp;
-            $('#avg_temp').html(avg_temp + '<sup>°C</sup>');
+            $("#avg_temp").html(avg_temp + "<sup>°C</sup>");
             // Set max temp
             max_temp = data.Month[0].max_temp;
-            $('#max_temp').html(max_temp + '<sup>°C</sup>');
+            $("#max_temp").html(max_temp + "<sup>°C</sup>");
 
             // Humidity chart
             var humdata = {
-                'data': humid_data,
-                'labels': labels
+                data: humid_data,
+                labels: labels,
             };
             charts.humdChart = createHumdChart(humdata);
 
             // Pressure chart
             var pressuredata = {
-                'data': pressure_data,
-                'labels': labels
-            }
+                data: pressure_data,
+                labels: labels,
+            };
             charts.pressureChart = createPressureChart(pressuredata);
-            
+
             /*// Crop yieds chart
             cropsdata = data.crops;
             charts.cropsChart = createCropsChart(cropsdata);
@@ -76,7 +78,7 @@ $(function () {
         },
         error: function (error) {
             console.log(error);
-        }
+        },
     });
 
     /**
@@ -111,4 +113,4 @@ $(function () {
         }
     }
     */
-})
+});
