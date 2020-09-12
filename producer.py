d2dbd760f111383e95e4f43168774db9b9e3192a
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-09-12 01:15:39 +0100

    Deleted Uvicorn image.

--- producer.py
+++ producer.py
@@ -1,14 +1,17 @@
+from time import sleep
 from kafka import KafkaProducer
 from json import dumps
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092', value_serializer=lambda msg: dumps(msg).encode('utf-8'))
 
-data = {
-    'Temperature': 23,
-    'Humidity': 40,
-    'Pressure': 20,
-    'pH': 6,
-}
-producer.send('WeatherData', data)
-producer.flush()
+while True:
+    data = {
+        'Temperature': 23,
+        'Humidity': 40,
+        'Pressure': 20,
+        'pH': 6,
+    }
+    producer.send('WeatherData', data)
+    producer.flush()
+    sleep(5)
